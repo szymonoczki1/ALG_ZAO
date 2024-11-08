b15ea39dea12357e8d8592d3d0e3dd1976dce829
szymonoczki1 <szymon.oczki1@gmail.com>
2024-11-08 13:19:14 +0100

    havelhakimialg and unit4task3

--- grafystepik.py
+++ grafystepik.py
@@ -9,20 +9,25 @@ class ReadVersion(Enum):
 class Graphs:
     def __init__(self) -> None:
         self.AdjacencyListAsDict = {}
-        self.Matrix = []
         self.DegreeSequence = []
         self.AmountOfNodes = 0
         self.AmountOfEdges = 0
         self.AverageDegree = 0
 
-    def GetAdjacencyListAsDict(self) -> dict[int, list[int]]:
-        return self.AdjacencyListAsDict
-    
-    def GetMatrix(self) -> list[list[int]]:
-        return self.Matrix
-    
-    def GetInOutDegList(self) -> list[list[int, int]]:
-        return self.InOutDegList
+        self.ReadAdjacencyList(ReadVersion.MANUAL)
+
+        self.CalculateAmountOfNodes()
+        self.CalculateDegreeSequence()
+        self.CalculateAmountOfEdges()
+        self.CalculateAverageDegree()
+
+        self.isACompleteGraph = self.CheckIfGraphIsComplete()
+        self.isACycle = self.CheckIfGraphIsACycle()
+        self.isAPath = self.CheckIfGraphIsAPath()
+        self.isATree = self.CheckIfGraphIsATree()
+        self.isAHypercube = self.CheckIfGraphIsAHypercube()
+        
+        print(self)
 
     def ReadAdjacencyList(self, version: ReadVersion) -> None:
         if version == ReadVersion.STEPIK:
@@ -52,10 +57,10 @@ class Graphs:
         if key in self.AdjacencyListAsDict:
             return len(self.AdjacencyListAsDict[key])
         else:
-            raise ValueError("key not in dictionary")
+            raise ValueError(f"{key} not in dictionary")
         
     def AreNodesConnected(self, node1: int, node2: int) -> bool:
-        """checks if nodes are connected, only works with adjacency list"""
+        """checks if nodes are connected, returns true or false, only works with adjacency list"""
         for node in (node1, node2):
             if node not in self.AdjacencyListAsDict:
                 raise ValueError(f"{node} doesnt exist")
@@ -66,11 +71,11 @@ class Graphs:
         self.AmountOfNodes = len(self.AdjacencyListAsDict)
     
     def CalculateAmountOfEdges(self) -> None:
-        self.AmountOfEdges = 0
-        for key, values in self.AdjacencyListAsDict.items():
-            for value in values:
-                if key < value:
-                    self.AmountOfEdges += 1
+        """Calculates amount of edges based on the formula degSum//2, degSum being the sum of every degree in the graph"""
+        degSum = 0
+        for deg in self.DegreeSequence:
+            degSum += deg
+        self.AmountOfEdges = degSum // 2
 
     def CalculateDegreeSequence(self) -> None:
         self.DegreeSequence = []
@@ -78,49 +83,68 @@ class Graphs:
             self.DegreeSequence.append(len(values))
 
     def CalculateAverageDegree(self) -> None:
-        degSum = 0
-        for values in self.AdjacencyListAsDict.values():
-            degSum += len(values)
-        self.AverageDegree = degSum/len(self.AdjacencyListAsDict)
+        for deg in self.DegreeSequence:
+            self.AverageDegree += deg
+        self.AverageDegree = self.AverageDegree / len(self.DegreeSequence)
 
     def CheckIfGraphIsComplete(self) -> bool:
-        pass
-
-
-    def PrintDegreeSequence(self) -> None:
-        print("Stopnie wierzchołków:", end='')
+        """Checks if the number of edges is equal to (n*(n-1))/2, n being the number of nodes"""
+        n = len(self.AdjacencyListAsDict)
+        expectedAmountOfEdges = (n*(n-1))/2
+        return self.AmountOfEdges == expectedAmountOfEdges
+    
+    def CheckIfGraphIsACycle(self) -> bool:
+        """Checks if every degree is equal to 2"""
         for deg in self.DegreeSequence:
-            print(f' {deg}', end='')
-        print()
+            if deg != 2:
+                return False
+        return True
+    
+    def CheckIfGraphIsAPath(self) -> bool:
+        """Checks if exactly 2 degrees are equal to 1 and all the other ones are equal to 2"""
+        degOne = 0
+        for deg in self.DegreeSequence:
+            if deg == 1:
+                degOne += 1
+            elif deg != 2:
+                return False
+        return degOne == 2
+    
+    def CheckIfGraphIsATree(self) -> bool:
+        """Checks if there is no isolated nodes and if the amount of edges equals to n-1, n being the number of nodes"""
+        noIsolatedNodes = True
+        for deg in self.DegreeSequence:
+            if deg == 0:
+                noIsolatedNodes = False
+        return self.AmountOfEdges == self.AmountOfNodes - 1 and noIsolatedNodes == True
+    
+    def CheckIfGraphIsAHypercube(self) -> bool:
+        """Checks if every degree is the same and if the amount of nodes is equal to 2^d, d being a degree"""
+        d = self.DegreeSequence[0]
+        return len(set(self.DegreeSequence)) == 1 and pow(2,d) == self.AmountOfNodes
 
-    def PrintAmountOfNodes(self) -> None:
-        print(f'Ilość wierzchołków: {self.AmountOfNodes}')
+    def __str__(self) -> str:
+        output = []
+        output.append(f'Ilość wierzchołków: {self.AmountOfNodes}')
+        output.append(f'Ilość krawędzi: {self.AmountOfEdges}')
+        output.append(f'Stopnie wierzchołków: {" ".join(map(str, self.DegreeSequence))}')
+        output.append(f'Średni stopień: {self.AverageDegree:.2f}')
 
-    def PrintAmountOfEdges(self) -> None:
-        print(f'Ilość krawędzi: {self.AmountOfEdges}')
+        graph_types = [(self.isACompleteGraph, "Jest to graf pełny"), (self.isACycle, "Jest to cykl"), (self.isAPath, "Jest to ścieżka"), (self.isATree, "Jest to drzewo"), (self.isAHypercube, "Jest to hiperkostka")]
 
-    def PringAverageDegree(self) -> None:
-        print(f'Średni stopień {self.AverageDegree:.2f}')
+        
+        counter = 0
+        for graph_type in graph_types:
+            if graph_type[0] == True:
+                output.append(graph_type[1])
+                counter += 1
 
-    def PrintDictAsAdjacencyList(self) -> None:
-        for key, value in self.AdjacencyListAsDict.items():
-            print(key, value)
+        if counter == 0:
+            output.append("Graf nie należy do żadnej z podstawowych klas.")
+
+        return '\n'.join(output)
 
 
 
-#inicjalizacja
-grafy = Graphs()
-#wczytaj liste
-grafy.ReadAdjacencyList(ReadVersion.MANUAL)
-#liczba wierzcholkow
-grafy.CalculateAmountOfNodes()
-grafy.PrintAmountOfNodes()
-#liczba krawedzi
-grafy.CalculateAmountOfEdges()
-grafy.PrintAmountOfEdges()
-#ciag stopni wierzcholkow nie posortowany
-grafy.CalculateDegreeSequence()
-grafy.PrintDegreeSequence()
-#sredni stopien grafu
-grafy.CalculateAverageDegree()
-grafy.PringAverageDegree()
+
+Graphs()
---
+++ havelhakimi.py
@@ -0,0 +1,24 @@
+def checkIfSequenceCanBeGraphic():
+    sequence = input()
+    sequence = list(map(int, sequence.split()))
+
+    sequence.sort(reverse=True)
+
+    while sequence:
+        firstDeg = sequence.pop(0)
+        if firstDeg > len(sequence):
+            return False
+
+        for i in range(firstDeg):
+            sequence[i] -= 1
+            if sequence[i] < 0:
+                return False
+        sequence.sort(reverse=True)
+    return True
+
+if checkIfSequenceCanBeGraphic() == True:
+    print('TAK')
+else:
+    print('NIE')
+
+#havel-hakimi alg
